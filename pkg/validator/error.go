7fd96b8779b59d3030c0cf9f329dcac841d1653e
TAKUYA MISHINA <TMISHINA@jp.ibm.com>
2026-07-16 16:07:34 +0900

    feat: gate ResourceClaim webhook on draDriver and VF mode
    Signed-off-by: TAKUYA MISHINA <TMISHINA@jp.ibm.com>

--- pkg/validator/error.go
+++ pkg/validator/error.go
@@ -17,6 +17,11 @@ var (
 	ErrNoSpyreScheduler         = errors.New("a pod must use \"schedulerName: spyre-scheduler\"")
 	ErrInvalidResourceAmount    = errors.New("a pod cannot request or limit Spyre devices for tier0, tier1, and tier2 with an odd number except 1")             //nolint:lll
 	ErrNodeNameWithScheduler    = errors.New("a pod must not use \".spec.nodeName\" with \"schedulerName: spyre-scheduler\"; use '.spec.nodeSelector' instead") //nolint:lll
+
+	ErrSpyrePFRestrictedToOperatorNamespace      = errors.New("spyre-pf DeviceClass is restricted to the spyre-operator namespace in VF mode")
+	ErrPrivilegedVFRestrictedToOperatorNamespace = errors.New("spyre-privileged-vf DeviceClass is restricted to the spyre-operator namespace")
+	ErrPrivilegedVFNotAllowedInNonVFMode         = errors.New("spyre-privileged-vf DeviceClass is not allowed when VF mode is disabled")
+	ErrStandardVFNotAllowedInNonVFMode           = errors.New("spyre-standard-vf DeviceClass is not allowed when VF mode is disabled")
 )
 
 func WrapConfigErr(err error) error {
